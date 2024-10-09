de4009fefa0f77a00c04be84f68258865d0d6b08
Fareda Elsayed <faredaelsayed0@gmail.com>
2024-10-09 17:49:07 +0300

    #	modified:   src/App.js #	modified:   src/CSS/Navbar.module.css #

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SearchForm from "./Components/SearchForm";
 import Navbar from "./Components/Navbar";
 import { SearchProvider } from "./Contexts/SearchContext";
 import { ThemeProvider } from "./Contexts/ThemeContext";
-
+import "./App.css"
 const App = () => {
   return (
     <ThemeProvider>
